bafd889bbdcf0c56cf7c1550b86da269ee2f9b56
Stoupy51 <stoupy51@gmail.com>
2022-09-21 18:14:13 +0200

    Better queue

--- INFO0501-AlgoAvancee/queue.h
+++ INFO0501-AlgoAvancee/queue.h
@@ -5,13 +5,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct queue Queue, *QUEUE;
-struct queue {
-	int * list;
+typedef struct queue {
+	int* list;
 	int head,
 		next,
 		size;
-};
+} Queue, *QUEUE;
+
+void initQueue(QUEUE q);
+void emfiler(QUEUE q, int v);
+int defiler(QUEUE q);
 
 #endif
 
