a1a8f21de7d37a89d4da882be62a24197c6350c0
Stoupy51 <stoupy51@gmail.com>
2022-10-16 19:34:41 +0200

    TP 3 Serveur web

--- INFO0503-Web/java/progWeb/src/prog_web/CodeDeSuivi.java
+++ INFO0503-Web/java/progWeb/src/prog_web/CodeDeSuivi.java
@@ -2,16 +2,40 @@ package prog_web;
 
 import org.json.JSONObject;
 
-public class CodeDeSuivi {
+public class CodeDeSuivi implements Comparable<CodeDeSuivi> {
     private String code;
+
+	private int energyType;
+    private float quantity;
+    private float minQuantity;
+    private int extractMode;
+	private String extractModeStrict;
+	private String energyFromCountry;
+    private float maxPrice;
+	private float maxBudget;
+	private int date;
+
    
     /**
-     * Crée un Code de suivi avec un code
+     * Crée un Code de suivi en utilisant le code fourni en paramètre
      * @param code le code
-     * @param dateDeNaissance la date de naissance
      */
     public CodeDeSuivi(String code) {
-        this.code = code;
+		this.code = code;
+		try {
+			String[] parts = code.split("-");
+			this.energyType = Integer.parseInt(parts[0]);
+			this.quantity = Float.parseFloat(parts[1]);
+			this.minQuantity = Float.parseFloat(parts[2]);
+			this.extractMode = Integer.parseInt(parts[3]);
+			this.extractModeStrict = parts[4];
+			this.energyFromCountry = parts[5];
+			this.maxPrice = Float.parseFloat(parts[6]);
+			this.maxBudget = Float.parseFloat(parts[7]);
+			this.date = Integer.parseInt(parts[8]);
+		} catch (Exception e) {
+			System.out.println("\nErreur lors de la création du code de suivi : CODE INVALIDE\n");
+		}
     }
     
     /**
@@ -20,9 +44,22 @@ public class CodeDeSuivi {
     public String getCode() {
         return code;
     }
+
+	/**
+	 * @return Getter pour tous les attributs
+	 */
+	public int getEnergyType() { return energyType; }
+	public float getQuantity() { return quantity; }
+	public float getMinQuantity() { return minQuantity; }
+	public int getExtractMode() { return extractMode; }
+	public String getExtractModeStrict() { return extractModeStrict; }
+	public String getEnergyFromCountry() { return energyFromCountry; }
+	public float getMaxPrice() { return maxPrice; }
+	public float getMaxBudget() { return maxBudget; }
+	public int getDate() { return date; }
     
     /**
-     * @return une chaine de caractères au format "code;code;code"
+     * @return une chaine de caractères au format "code-code-...-code"
      */
     @Override
     public String toString() {
@@ -30,11 +67,20 @@ public class CodeDeSuivi {
     }
 
     /**
-     * @return l'auteur converti en objet JSON
+     * @return le code converti en objet JSON
      */
     public JSONObject toJSON() {
         JSONObject objet = new JSONObject();
         objet.put("code", code);
+		objet.put("energyType", energyType);
+		objet.put("quantity", quantity);
+		objet.put("minQuantity", minQuantity);
+		objet.put("extractMode", extractMode);
+		objet.put("extractModeStrict", extractModeStrict);
+		objet.put("energyFromCountry", energyFromCountry);
+		objet.put("maxPrice", maxPrice);
+		objet.put("maxBudget", maxBudget);
+		objet.put("date", date);
         return objet;
     }
 
@@ -43,9 +89,32 @@ public class CodeDeSuivi {
      * @param json
      */
     public static CodeDeSuivi fromJSON(JSONObject json) {
-        return new CodeDeSuivi(
-            json.getString("code")
-        );
+		String code = Integer.toString(json.getInt("energyType"));
+		code += "-" + Float.toString(json.getFloat("quantity"));
+		code += "-" + Float.toString(json.getFloat("minQuantity"));
+		code += "-" + Integer.toString(json.getInt("extractMode"));
+		code += "-" + json.get("extractModeStrict");
+		code += "-" + json.getString("energyFromCountry");
+		code += "-" + Float.toString(json.getFloat("maxPrice"));
+		code += "-" + Float.toString(json.getFloat("maxBudget"));
+		code += "-" + Integer.toString(json.getInt("date"));
+        return new CodeDeSuivi(code);
     }
+
+	/**
+     * @return 1 si la quantité du premier code est supérieure à celle du second
+	 * @return -1 si la quantité du premier code est inférieure à celle du second
+	 * @return 0 si les deux codes ont la même quantité
+	 */
+	@Override
+	public int compareTo(CodeDeSuivi code) {
+		if (this.quantity > code.quantity) {
+			return 1;
+		}
+		if (this.quantity < code.quantity) {
+			return -1;
+		}
+		return 0;
+	}
 }
 
--- INFO0503-Web/java/progWeb/src/prog_web/main_prog.java
+++ INFO0503-Web/java/progWeb/src/prog_web/main_prog.java
@@ -1,19 +1,46 @@
 
 package prog_web;
 
-import java.security.Timestamp;
-
 public class main_prog {
     public main_prog() {}
 
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\n\n\n\nDébut de la fonction main()\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nDébut de la fonction main()\n");
         Long time = System.currentTimeMillis();
-        
-        CodeDeSuivi code = new CodeDeSuivi("51;515;15");
+
+
+		// construire votre objet à partir des caractéristiques indépendantes du code;
+        CodeDeSuivi code = new CodeDeSuivi("0-2573.0-1200.0-3-true-FR-0.1546-400.0-1665232943");
         System.out.println("\n"+code+"\n");
 
-        System.out.println("\nFin de la fonction main() en "+toString(System.currentTimeMillis()-time))+"ms\n");
+		// récupérer une caractéristiques en particuliers;
+		System.out.println("\nenergyType: "+Integer.toString(code.getEnergyType()));
+		System.out.println("quantity: "+Float.toString(code.getQuantity()));
+		System.out.println("minQuantity: "+Float.toString(code.getMinQuantity()));
+		System.out.println("extractMode: "+Integer.toString(code.getExtractMode()));
+		System.out.println("extractModeStrict: "+code.getExtractModeStrict());
+		System.out.println("energyFromCountry: "+code.getEnergyFromCountry());
+		System.out.println("maxPrice: "+Float.toString(code.getMaxPrice()));
+		System.out.println("maxBudget: "+Float.toString(code.getMaxBudget()));
+		System.out.println("date: "+Integer.toString(code.getDate()));
+
+		// transformer votre objet en String JSON;
+		System.out.println("\nJSON: "+code.toJSON()+"\n");
+
+		// reconstruire un objet CodeDeSuivi depuis un String JSON;
+		CodeDeSuivi code2 = CodeDeSuivi.fromJSON(code.toJSON());
+		System.out.println("\n"+code2+"\n");
+
+		// trier des instances de CodeDeSuivi selon la quantité d'énergie qu'il décrive.
+		// Pour ce faire, on implémentera l'interface Comparable.
+		CodeDeSuivi[] codes = {code2, new CodeDeSuivi("0-2700.0-1200.0-3-true-FR-0.1546-400.0-1665232943"), new CodeDeSuivi("0-2300.0-1200.0-3-true-FR-0.1546-400.0-1665232943")};
+		System.out.println("\nAvant le tri : "+codes[0]+", "+codes[1]+", "+codes[2]);
+		java.util.Arrays.sort(codes);
+		System.out.println("Après le tri : "+codes[0]+", "+codes[1]+", "+codes[2]);
+
+
+
+        System.out.println("\n\nFin de la fonction main() en "+Long.toString((System.currentTimeMillis()-time))+"ms\n");
     }
 }
 
