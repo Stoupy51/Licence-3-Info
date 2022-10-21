414c3f64ec76f1666b6bf329787443d1401af5ec
Stoupy51 <stoupy51@gmail.com>
2022-10-21 15:03:34 +0200

    Prise en main HTTP en Java

---
+++ INFO0503-Web/Fiche_01_HttpServer/srv/IndexHandler.java
@@ -0,0 +1,47 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.Headers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Classe correspondant au handler sur le contexte 'index.html'.
+ * @author Alexandre Collignon
+ */
+class IndexHandler implements HttpHandler {
+
+    public void handle(HttpExchange t) {
+        int rCode = 200;
+        String reponse = "";
+        try {
+            reponse += Files.readString(Path.of("srv/testServeurHttpSimple.html"));
+        } catch (IOException e) {
+            System.err.println("Fichier introuvable : " + e);
+            reponse = "Erreur 404: Fichier Introuvable";
+            rCode = 404;
+        }
+
+        // Envoi de l'en-tête Http
+        try {
+            Headers h = t.getResponseHeaders();
+            h.set("Content-Type", "text/html; charset=utf-8");
+            t.sendResponseHeaders(rCode, reponse.getBytes().length);
+        } catch(IOException e) {
+            System.err.println("Erreur lors de l'envoi de l'en-tête : " + e);
+            System.exit(0);
+        }
+
+        // Envoi du corps (données HTML)
+        try {
+            OutputStream os = t.getResponseBody();
+            os.write(reponse.getBytes());
+            os.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de l'envoi du corps : " + e);
+        }
+    }
+}
+
---
+++ INFO0503-Web/Fiche_01_HttpServer/srv/Readme.txt
@@ -0,0 +1,5 @@
+1) Compilez les classes :
+  > javac *.java
+2) Executez le serveur :
+  > java ServeurHttpSimple
+3) Ouvrez le fichier testServeurHttpSimple.html dans un navigateur Internet puis testez les deux boutons
