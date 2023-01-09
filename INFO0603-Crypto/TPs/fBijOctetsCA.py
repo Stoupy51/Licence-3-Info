3ffcaedb0d2d78c6cd582e0c43053ae4b7cd51a3
Stoupy51 <stoupy51@gmail.com>
2023-01-09 22:34:39 +0100

    Pet couil

--- INFO0603-Crypto/TPs/fBijOctetsCA.py
+++ INFO0603-Crypto/TPs/fBijOctetsCA.py
@@ -42,17 +42,32 @@ class fFeistel(object):
         self.lk = [randint(0, 255) for _ in range(n)]
         self.f = f
         self.n = n
-    
+
     def __repr__(self):
         return f"fFeistel({self.k}, {self.f.__repr__()}, {self.n})"
-    
+
     def __call__(self, octet):
-        l = octet & 0xF0
-        r = octet & 0x0F
-        for t in range(self.n):
-            l, r = self.lk[t], self.f(r ^ self.lk[t]).rep ^ l
-            octet = self.f(r ^ self.k).rep ^ l
+        left = octet & 0xF0
+        right = octet & 0x0F
+        print(f"octet, lk = {octet}, {self.lk}")
+        for i in range(self.n):
+            left, right = right, left ^ self.f(right).rep #^ self.lk[i]
+        print(f"left,right = {left},{right}")
+        octet = left ^ right
         return octet
+
+    def valInv(self, octet):
+        """Fonction inverse de __call__
+        >>> fFeistel().valInv(fFeistel()(51))
+        51
+        """
+        left = octet & 0xF0
+        right = octet & 0x0F
+        print(f"octet, lk = {octet}, {self.lk}")
+        for i in range(self.n):
+            left, right = right, left ^ self.f.valInv(right).rep #^ self.lk[i]
+        print(f"left,right = {left},{right}")
+        return left ^ right
 
 
 
@@ -76,12 +91,19 @@ if __name__ == "__main__":
     plt.show()
 
     # Feistel
-    f = fFeistel(5, f, 1)
-    lx = [i for i in range(256)]
-    ly = [f(i) for i in lx]
-    plt.plot(lx, ly, '*')
-    plt.title(f"Graphique de {f}")
-    plt.show()
+    for c in range(1,8):
+        f = fFeistel(5, fBijAffine(7, 1347), c)
+        lx = [i for i in range(256)]
+        ly = [f(i) for i in lx]
+        plt.plot(lx, ly, '*')
+        plt.title(f"Graphique de {f}")
+        plt.show()
+    
+    for i in [0x0F, 0xF0, 0xFF, 0xC5]:
+        f = fFeistel(5, fBijAffine(7, 1347), 7)
+        c = f(i)
+        d = f.valInv(c)
+        print(f"c,d,i = {c},{d},{i}\n")
 
 
 
