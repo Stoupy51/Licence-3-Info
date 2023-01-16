711c80789d5f90dc0396149b84b6ddc0a5c0ab90
Stoupy51 <stoupy51@gmail.com>
2023-01-16 21:12:58 +0100

    Crack

--- INFO0603-Crypto/TPs/Rendus/crack.py
+++ INFO0603-Crypto/TPs/Rendus/crack.py
@@ -7,17 +7,39 @@ from arithmetiqueDansZ import *
 from Binaire603 import Binaire603
 from ChiffreurAffine import ChiffreurAffine
 from ChiffreurVigenere import ChiffreurVigenere
-from ChiffreurParDecalage import ChiffreurParDecalage
+
+def crackAffineBruteForce(lb: Binaire603, fFile: str = "Les Miserables.txt"):
+    lf = lb.lFrequences()
+    Ysp = lf.index(max(lf))
+    lf.pop(Ysp)
+    Ye = lf.index(max(lf))
+
+    fr = Binaire603.bin603DepuisFichier(fFile).lFrequences()
+    Xsp = fr.index(max(fr))
+    fr.pop(Xsp)
+    Xe = fr.index(max(fr))
+    
+    print(f"Xsp = {Xsp}, Ysp = {Ysp}, Xe = {Xe}, Ye = {Ye}")
+    ### Tester tous les combinaisons de a et calculer b
+    for a in range(1, 256, 2):
+        b = (Ysp - a * Xsp) % 256
+
+        chiffreur = ChiffreurAffine(a,b)
+        if (chiffreur.a.estInversible()):
+            ldf = chiffreur.binDecode(lb).lFrequences()
+            
+            Dsp = ldf.index(max(ldf))
+            ldf.pop(Dsp)
+            De = ldf.index(max(ldf))
+        
+            print(f"Dsp = {Dsp}, De = {De}, {chiffreur}")
+            if (Xsp == Dsp and Xe == De):
+                return chiffreur
+    return ChiffreurAffine(-1,-1)
+
 
 def crackAffine(lb: Binaire603, fFile: str = "Les Miserables.txt"):
     lf = lb.lFrequences()
-
-    ### Tester tous les combinaisons de a et b
-    #for a in range(1, 256, 3):
-    #    for b in range(256):
-    #        chiffreur = ChiffreurAffine(a,b)
-    #        ldf = chiffreur.binDecode(lb).lFrequences()
-    #        distance = 0
     
     # Méthode pour comparer les fréquences du français
     # avec le texte codé
@@ -53,7 +75,7 @@ if __name__ == "__main__":
     print(c1.binDecode(f1).toString())
     
     f2 = Binaire603.bin603DepuisFichier("Chiffre2.TXT")
-    c2 = crackAffine(f2)
+    c2 = crackAffineBruteForce(f2)
     print(c2)
     print(c2.binDecode(f2).toString())
     
