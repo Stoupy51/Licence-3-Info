4906e7634324a230fe809bc3b1d88025cde81fc7
Stoupy51 <stoupy51@gmail.com>
2023-01-19 20:17:22 +0000

    Send to jesus

--- BenchmarkC/main.c
+++ BenchmarkC/main.c
@@ -6,21 +6,26 @@
 #include <sys/time.h>
 #include <time.h>
 
+#define COLOR_RESET "\033[0m"
+#define COLOR_RED "\033[0;31m"
+#define COLOR_YELLOW "\033[0;33m"
+
 long BENCH_countF1 = 0;
 long BENCH_countF2 = 0;
+time_t BENCH_start = 0;
 time_t BENCH_end = 0;
 
-#define BENCHMARK_BETWEEN(f1, f2, testing_time) \
+#define BENCHMARK_BETWEEN(bench_f1, bench_f2, bench_testing_time) \
 	BENCH_countF1 = 0; \
 	BENCH_countF2 = 0; \
-	BENCH_end = time(NULL) + testing_time; \
+	BENCH_end = time(NULL) + bench_testing_time; \
 	while (time(NULL) < BENCH_end) { \
-		f1; \
+		bench_f1; \
 		BENCH_countF1 += 1; \
 	} \
-	BENCH_end = time(NULL) + testing_time; \
+	BENCH_end = time(NULL) + bench_testing_time; \
 	while (time(NULL) < BENCH_end) { \
-		f2; \
+		bench_f2; \
 		BENCH_countF2 += 1; \
 	} \
 	if (BENCH_countF1 > BENCH_countF2) { \
@@ -28,7 +33,16 @@ time_t BENCH_end = 0;
 	} else { \
 		printf("\nf1 < f2 by %f times with", (double)BENCH_countF2 / (double)BENCH_countF1); \
 	} \
-	printf("\nf1 executed %ld times and f2 executed %ld times\n\n", BENCH_countF1, BENCH_countF2);
+	printf("\n%sf1 executed %s%ld%s times and f2 executed %s%ld times%s\n\n", COLOR_RED, COLOR_YELLOW, COLOR_RED, BENCH_countF1, COLOR_YELLOW, BENCH_countF2, COLOR_RESET);
+
+#define BENCHMARK_SOLO(bench_f, bench_count) \
+	BENCH_start = time(NULL); \
+	for (int i = 0; i < bench_count; i++) { \
+		bench_f; \
+	} \
+	BENCH_end = time(NULL); \
+	printf("\nbench_f executed %d times in %ld seconds\n\n", bench_count, BENCH_end - BENCH_start);
+
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
