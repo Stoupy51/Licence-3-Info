f68dccbd62ffcfcb75a3dc6c00e03028c6b80610
Stoupy51 <stoupy51@gmail.com>
2023-03-15 19:26:45 +0100

    Clean files

--- INFO0603-Crypto/TPs/tp_primes_write.py
+++ INFO0603-Crypto/TPs/tp_primes_write.py
@@ -2,11 +2,11 @@
 from arithmetiqueDansZ import *
 
 l = [2]
-for k in range(3, 2**24, 2):
+for k in range(3, 2**16, 2):
     if isprime(k):
         l.append(k)
         if len(l) % 10000 == 0:
-            f = open("tp_primes.py", "w")
+            f = open("./tp_primes.py", "w")
             f.write(f"pLen = {len(l)}\n")
             f.write(f"p = {l}")
             f.close()
