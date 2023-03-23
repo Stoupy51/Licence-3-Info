3af94e508d6f107a05d69369ddc3049e9d6d8fcf
Stoupy51 <stoupy51@gmail.com>
2023-03-23 16:08:25 +0100

    Rip

--- INFO0606-ImagerieNumerique/TP3/Assets/Scripts/LestCommand.cs
+++ INFO0606-ImagerieNumerique/TP3/Assets/Scripts/LestCommand.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class LestCommand : MonoBehaviour {
 	// Public variables (pattes de la grue en anglais)
 	public GameObject[] paws;
+	public bool[] pawsDeployed;
 	public GameObject[] pawsChild;
 	public GameObject[] pawsChildChild;
 	
 	// Private variables
-	private Boolean moving = false;
+	private bool moving = false;
 	private int deployedState = 0;
+	private float deployingTime = 2;
 
 	// Start is called before the first frame update
 	void Start() {
+		// Initialize the pawsDeployed array
+		this.pawsDeployed = new bool[4];
+		for (int i = 0; i < 4; i++) {
+			this.pawsDeployed[i] = false;
+		}
+
 		// Initialize the pawsChild and pawsChildChild arrays
 		this.pawsChild = new GameObject[4];
 		this.pawsChildChild = new GameObject[4];
@@ -29,6 +37,7 @@ public class LestCommand : MonoBehaviour {
 
 		// If player press 'D', start deploying the paws
 		if (Input.GetKeyDown(KeyCode.D)) {
+			Debug.Log("D pressed");
 			this.moving = true;
 		}
 
@@ -36,44 +45,53 @@ public class LestCommand : MonoBehaviour {
 		if (this.moving) {
 			// For each paws, rotate Z axis by 135 degree (but -135 for the second and third paws)
 			if (this.deployedState == 0) {
-				
-				// Number of paws finished to deploy
-				int numberDeployed = 4;
 
-				// First paw
-				if (this.paws[0].transform.rotation.z < 135) {
-					this.paws[0].transform.Rotate(0, 0, 135 * Time.deltaTime);
-					numberDeployed--;
-				}
+				// Rotate the paws
+				for (int i = 0; i < 4; i++) {
+					// If the paw is not deployed
+					if (!this.pawsDeployed[i]) {
 
-				// Second paw
-				if (this.paws[1].transform.rotation.z > -135) {
-					this.paws[1].transform.Rotate(0, 0, -135 * Time.deltaTime);
-					numberDeployed--;
-				}
+						// Get the articulation body of the paw
+						ArticulationBody articulation = this.paws[i].GetComponent<ArticulationBody>();
 
-				// Third paw
-				if (this.paws[2].transform.rotation.z > -135) {
-					this.paws[2].transform.Rotate(0, 0, -135 * Time.deltaTime);
-					numberDeployed--;
-				}
-
-				// Fourth paw
-				if (this.paws[3].transform.rotation.z < 135) {
-					this.paws[3].transform.Rotate(0, 0, 135 * Time.deltaTime);
-					numberDeployed--;
+						if (i == 0 || i == 3) {
+							articulation.zDrive = new ArticulationDrive {
+								lowerLimit = articulation.zDrive.lowerLimit - 135 * Time.deltaTime,
+								upperLimit = articulation.zDrive.upperLimit + 135 * Time.deltaTime
+							};
+							this.pawsDeployed[i] = (articulation.zDrive.lowerLimit <= -135);
+						}
+						else {						
+							articulation.zDrive = new ArticulationDrive {
+								lowerLimit = articulation.zDrive.lowerLimit + 135 * Time.deltaTime,
+								upperLimit = articulation.zDrive.upperLimit - 135 * Time.deltaTime
+							};
+							this.pawsDeployed[i] = (articulation.zDrive.lowerLimit >= 135);
+						}
+					}
 				}
 				
-				// If all the paws are deployed, change the state
-				if (numberDeployed == 4) {
-					this.deployedState = 4;
+				// If all the paws are deployed, change the state and set the deploying time
+				bool allDeployed = true;
+				for (int i = 0; i < 4; i++)
+					allDeployed = allDeployed && this.pawsDeployed[i];
+
+				if (allDeployed) {
+					Debug.Log("All paws deployed, changing state to 1");
+					this.deployedState = 1;
+					this.deployingTime = 2;
 				}
 			}
 
 			// For each paws, rotate X axis by -90 degree their first child and by 90 degree their child's child
-			else if (this.deployedState >= 2) {
+			else if (this.deployedState == 1) {
 				// Deploy for 2 seconds
-				this.deployedState -= Time.deltaTime;
+				this.deployingTime -= Time.deltaTime;
+				if (this.deployingTime <= 0) {
+					Debug.Log("Deploying time finished, changing state to 2");
+					this.deployedState = 2;
+					this.moving = false;
+				}
 
 				// Rotate the paws
 				for (int i = 0; i < 4; i++) {
